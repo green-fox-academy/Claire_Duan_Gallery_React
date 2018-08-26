ec68915502b7cf5764d18d1b27abb1c649e23789
Claire_Duan@epam.com <Claire_Duan@epam.com>
2018-08-26 15:08:24 +0800

    Push test

--- src/containers/ImageShow.js
+++ src/containers/ImageShow.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import PictureDisplay from '../components/PictureDisplay';
 
-// Push test
 const mapStateToProps = (state) => {
   const { images, currentIndex } = state;
   return {
